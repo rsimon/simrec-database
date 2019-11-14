33ebd8c1685524f2feff02994c7329b2bbf6d184
Rainer Simon <rainer.simon@ait.ac.at>
2019-11-14 10:22:22 +0100

    Basic plumbing: PHP JSON API to map rendering

--- map/src/App.js
+++ map/src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Map, TileLayer, GeoJSON, Popup } from 'react-leaflet';
 
 import './App.css';
 
 const position = [51.505, -0.09]
 
-function App() {
+export default class App extends Component {
 
-  return (
-    <Map center={position} zoom={13}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-      />
-      <Marker position={position}>
-        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-      </Marker>
-    </Map>
-  );
+  state = {
+    routes: []
+  }
+
+  // For testing
+  componentDidMount() {
+    axios.get('/api/routes').then(response => {
+      this.setState({
+        routes: response.data.map(r => r.geom_kml)
+      });
+    });
+  }
+
+  render() {
+    return (
+      <Map center={position} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" />
+
+        { this.state.routes.map((route, idx) => 
+          <GeoJSON key={idx} data={route}>
+            <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+          </GeoJSON>
+        )}        
+      </Map>
+    )
+  }
 
 }
-
-export default App;
