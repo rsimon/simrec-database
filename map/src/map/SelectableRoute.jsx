057a63bc64e22fa2f1a35d37308fc01786b6b1f3
Rainer Simon <rainer.simon@ait.ac.at>
2019-11-15 10:27:12 +0100

    Route styling

--- map/src/map/SelectableRoute.jsx
+++ map/src/map/SelectableRoute.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { GeoJSON, Popup } from 'react-leaflet';
 import { CardContent, Typography } from '@material-ui/core';
 
+const ROUTE_STYLES = {
+  PRIMARY:   _ => ({ weight: 5, color: 'rgb(204, 8, 8)' }),
+  SECONDARY: _ => ({ weight: 3, color: 'rgba(204, 8, 8, 0.65)' })
+}
+
 const SelectableRoute = props => {
 
   const init = ref => {
@@ -9,10 +14,15 @@ const SelectableRoute = props => {
   }
   
   return (
-    <GeoJSON ref={init} data={props.geom_kml} onClick={props.onClick}>
+    <GeoJSON 
+      ref={init} 
+      data={props.geom_kml} 
+      onClick={props.onClick}
+      style={ROUTE_STYLES[props.route_type]}>
+
       <Popup>
         <Typography>
-          <a href="#">{props.id}</a>
+          <a href={`crud/routes/read/${props.id}`}>{props.id}</a>
         </Typography>
 
         <Typography variant="body2" component="p">
